2c1f5ce1655d740e1493b4bd3a4b4883c1e2973a
KanishkasreeR <kanishkasree.r2022cse@sece.ac.in>
2025-07-26 14:46:15 +0530

    Task 1

---
+++ verify_installation.py
@@ -0,0 +1,13 @@
+import pytest
+import selenium
+import requests
+import pandas
+import openpyxl
+
+print("Installed Package Versions:\n")
+
+print(f"pytest: {pytest.__version__}")
+print(f"selenium: {selenium.__version__}")
+print(f"requests: {requests.__version__}")
+print(f"pandas: {pandas.__version__}")
+print(f"openpyxl: {openpyxl.__version__}")
